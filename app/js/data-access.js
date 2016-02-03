477ddf86c4ef59888604860325e47ae3ab22635d
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-03 10:45:17 -0500

    Download state boundaries geojson

--- app/js/data-access.js
+++ app/js/data-access.js
@@ -4,12 +4,19 @@
   var xhr = require('xhr');
   var emitter = require('./mediator');
 
-  var accomplishments;
+  var accomplishments, states;
 
   function init() {
     xhr.get('./data/accomplishments.js', function (err, res) {
+      if (err) console.error(err);
       accomplishments = JSON.parse(res.body);
       emitter.emit('accomplishments:loaded', accomplishments);
+    });
+
+    xhr.get('./data/southeast.js', function (err, res) {
+      if (err) console.error(err);
+      states = JSON.parse(res.body);
+      emitter.emit('states:loaded', states);
     });
   }
 
@@ -17,6 +24,11 @@
     return accomplishments;
   }
 
+  function getStates() {
+    return states;
+  }
+
   module.exports.init = init;
   module.exports.getAll = getAll;
+  module.exports.getStates = getStates;
 })();
