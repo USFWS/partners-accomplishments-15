546c082f8708658dbac5cd8b8f022c3818bc8419
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-03 10:47:27 -0500

    Display state boundary when a state is selected

--- app/js/map.js
+++ app/js/map.js
@@ -4,6 +4,9 @@
   var L = require('leaflet');
   require('./leaflet.bouncemarker');
   var emitter = require('./mediator');
+  var _ = {
+    defaults: require('lodash.defaults')
+  };
   var icon = L.icon({
     iconUrl: './img/marker.svg',
     iconSize: [10,10]
@@ -11,7 +14,8 @@
 
   L.Icon.Default.imagePath = './img/';
 
-  var map, options, layers, layerGroup,
+  var map, layers, boundaries, layerGroup, stateGroup,
+      options = {},
       southeast = [
         [16.155864, -62.779455],
         [39.795251, -95.562656]
@@ -19,10 +23,10 @@
 
   function init(opts) {
     if (!opts.data) throw 'You must provide the map with some data.';
-    options = opts;
+    options = _.defaults(options, opts);
     createMap();
-    createLayers();
     registerHandlers();
+    createLayers();
   }
 
   function registerHandlers() {
@@ -32,10 +36,23 @@
   function advanceSlide(anchor) {
     var href = anchor.getAttribute('href').slice(1);
     layerGroup.clearLayers();
+    stateGroup.clearLayers();
+    stateGroup.addLayer(boundaries[href]);
     setTimeout(function() {
       layerGroup.addLayer(layers[href]);
     }, 1200);
     map.flyToBounds(layers[href]);
+  }
+
+  function createStateBoundary(state) {
+    return L.geoJson(options.states, {
+      filter: function (feature, latlng) {
+        switch (feature.properties.NAME) {
+          case state: return true;
+          default: return false;
+        }
+      }
+    });
   }
 
   function createStateLayer(state) {
@@ -73,9 +90,7 @@
       TN: createStateLayer('Tennessee'),
       KY: createStateLayer('Kentucky'),
     };
-
     layerGroup.addLayer(layers.allPoints);
-
     map.fitBounds(layers.allPoints.getBounds());
   }
 
@@ -87,9 +102,28 @@
     });
 
     layerGroup = L.layerGroup().addTo(map);
+    stateGroup = L.layerGroup().addTo(map);
 
     L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png').addTo(map);
   }
 
+  function addStates(states) {
+    options.states = states;
+
+    boundaries = {
+      AL: createStateBoundary('AL'),
+      AR: createStateBoundary('AR'),
+      FL: createStateBoundary('FL'),
+      GA: createStateBoundary('GA'),
+      NC: createStateBoundary('NC'),
+      PR: createStateBoundary('PR'),
+      LA: createStateBoundary('LA'),
+      MS: createStateBoundary('MS'),
+      TN: createStateBoundary('TN'),
+      KY: createStateBoundary('KY'),
+    };
+  }
+
   module.exports.init = init;
+  module.exports.addStates = addStates;
 })();
